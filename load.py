267338581773f182c8ec8defae39ee2fb4ab034e
Hangyul Yi <hangyulyi3@gmail.com>
2024-06-02 23:10:51 -0600

    data.json with all data needed

--- load.py
+++ load.py
@@ -1,18 +1,27 @@
 import openpyxl
 import json
+import os
 
-workbook = openpyxl.load_workbook('leniverse/1.xlsx')
-worksheet = workbook.active
 
 data_list = []
 
-for row in worksheet.iter_rows(min_row=2, max_row=worksheet.max_row, min_col=3, max_col=4, values_only=True):
-    data_list.append({
-        'kr': row[0],
-        'eng': row[1]
-    })
+directory = 'leniverse'
+
+for filename in os.listdir(directory):
+    if filename.endswith('.xlsx'):
+        file_path = os.path.join(directory, filename)
+
+        workbook = openpyxl.load_workbook(file_path)
+        worksheet = workbook.active
+
+        for row in worksheet.iter_rows(min_row=2, max_row=worksheet.max_row, min_col=3, max_col=4, values_only=True):
+            data_list.append({
+                'kr': row[0],
+                'eng': row[1]
+            })
 
 json_data = json.dumps(data_list, ensure_ascii=False, indent=4)
 
 with open('data.json', 'w', encoding='utf-8') as f:
     f.write(json_data)
+
