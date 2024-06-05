a43cffb018a49daf55914fe78a5a8c009d95ba2f
Hangyul Yi <hangyulyi3@gmail.com>
2024-06-04 19:40:24 -0600

    update comments

--- xlsx-to-json.py
+++ xlsx-to-json.py
@@ -5,7 +5,8 @@ import os
 
 data_list = []
 
-directory = 'leniverse'
+# insert name of folder you wish to iterate through with excel files
+directory = ''
 
 for filename in os.listdir(directory):
     if filename.endswith('.xlsx'):
@@ -14,6 +15,8 @@ for filename in os.listdir(directory):
         workbook = openpyxl.load_workbook(file_path)
         worksheet = workbook.active
 
+        # adjust row/col accordingly, will grab from two different columns and all the rows
+        # Can refer to xlsx-to-json-copy.py file for more comments if needed
         for row in worksheet.iter_rows(min_row=2, max_row=worksheet.max_row, min_col=3, max_col=4, values_only=True):
             data_list.append({
                 'kr': row[0],
